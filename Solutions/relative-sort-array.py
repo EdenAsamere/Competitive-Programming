57edfc0909fb5690618af3c913172e6ebd54242c
Eden Asamere <126487966+EdenAsamere@users.noreply.github.com>
2023-12-27 08:13:25 -0800

    Added solution for Relative Sort Array

--- Solutions/relative-sort-array.py
+++ Solutions/relative-sort-array.py
@@ -1,12 +1,23 @@
 from collections import Counter
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        hashmap = Counter(arr1)
-        output = []
-        for i in arr2:
-            output.extend([i]*hashmap[i])
-        diff = sorted(list( set(arr1) - set(arr2)))
-        for i in diff:
-            output.extend([i]*hashmap[i])
-        return output
+        hashmap = {j: i for i, j in enumerate(arr2)}
+        def customComparator(item):
+            return hashmap[item] if item in hashmap else len(arr2) + item
+        arr1.sort(key=customComparator)
+        return arr1
+
+        
+        
+        
+        
+        
+        # hashmap = Counter(arr1)
+        # output = []
+        # for i in arr2:
+        #     output.extend([i]*hashmap[i])
+        # diff = sorted(list( set(arr1) - set(arr2)))
+        # for i in diff:
+        #     output.extend([i]*hashmap[i])
+        # return output
         
